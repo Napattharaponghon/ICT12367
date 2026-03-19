7fdf1f8c7ddf7b9f140a62311f00b4adfd02f92a
Napattharaponghon <napattharapong.hon@spumail.net>
2026-03-19 14:38:05 +0700

    Add files via upload

---
+++ Lab_11 68105233/myproject/myapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from myapp.models import Person
+
+# Create your views here.
+def index(request):
+    all_person = Person.objects.all()
+    return render(request, 'index.html', {"all_person": all_person})
+
+def about(request):
+    return render(request, 'about.html')
+
+def form(request):
+    if request.method == "POST":
+        # รับข้อมูลจากฟอร์ม
+        name = request.POST.get("name")
+        age = int(request.POST.get("age"))
+
+        # บันทึกข้อมูลลงฐานข้อมูล
+        person = Person.objects.create(
+            name=name,
+            age=age
+        )
+
+        # เปลี่ยเส้นทางไปหน้าแรก
+        return redirect("/")
+    else:
+        # แสดงฟอร์ม
+        return render(request, 'form.html')
+
